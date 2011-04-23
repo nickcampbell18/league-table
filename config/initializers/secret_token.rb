e253358217932d89a1e5fd75c81a5786344aa104
Nick Campbell <nickcampbell18@gmail.com>
2011-04-23 18:12:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeagueTable::Application.config.secret_token = '3d0f58b11a4cfe758de631506f23198b2b747f8e62d656fc500c26c2b202f39e7bb59af0c8f09f159dd78b34f9369814fd565c67fbcc89f2ec80ec05276a3500'
