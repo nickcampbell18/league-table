e41e2400979303a0fd358c67c3e5638ec8545515
Nick Campbell <nickcampbell18@gmail.com>
2011-04-25 19:53:13 +0100

    Added base navigation html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def navigation
+    html = ''
+    case params[:controller]
+    when "teams"
+      html << clubsnav
+        html << content_tag(:li, link_to("Team "+@team.name, url_for([@club, @team])))
+    when "clubs"
+      html << clubsnav
+    end
+    return raw(html)
+  end
+
+  def clubsnav
+    html = content_tag(:li, link_to("Clubs", url_for(:clubs)))
+    if params[:id]
+      html << content_tag(:li, link_to(@club.name, url_for(@club)))
+    end
+    return html
+  end
 end
